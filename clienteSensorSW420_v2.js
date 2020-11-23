38195e21db7913c7eb001aa03977d2ad8676e1cd
marcelagil <marcela.gil@usach.cl>
2020-11-22 22:01:34 -0300

    Create clienteSensorSW420_v2.js

---
+++ clienteSensorSW420_v2.js
@@ -0,0 +1,100 @@
+// Always use JavaScript strict mode.
+"use strict";
+
+// Modules required here
+var wpi = require('node-wiring-pi');
+
+var sensor = 0;
+wpi.setup('wpi');
+wpi.pinMode(sensor, wpi.INPUT);
+var seqNo = 0;
+var maxMediciones=100;
+
+function TestVibration(){
+	var status;
+	let ts = Date.now();	
+	if(wpi.digitalRead(sensor) == 1){
+		
+		console.log("Movement Detected");
+		status = 1;
+        var TLSClient = require('./tls-client.js');
+        var c1 = new TLSClient('agent1', 8000);
+        c1.on('connect', function (err) {
+            console.log('Cliente conectado-sensor conectado');
+            seqNo= seqNo+1;
+			console.log(seqNo);
+			if (seqNo>maxMediciones) {
+				process.exit();
+			}
+			var medicion= {
+				"sensor" :{
+					"id":2,
+					"tipo":"SW420"
+				},
+				"dispositivo":{
+					"id":2,
+					"tipo":"Raspberry Pi 3"
+				},
+				"fecha" : new Date(),
+				"medida": {
+					"Movimiento": 1,
+				},
+				"alerta" : true,
+				"observaciones" : ""
+			};
+			console.log("segNo: ", medicion); 
+			c1.write (medicion);
+        });
+   
+        c1.on('disconnect', function (err) {
+            console.log('Servidor desconectado o error en la conexión');
+        });
+        console.log('STARTED');
+		
+	}else{
+		console.log("no Movement Detected");
+		status = 0;
+			console.log("No se detecta movimiento");
+        var TLSClient = require('./tls-client.js');
+        var c1 = new TLSClient('agent1', 8000);        
+        c1.on('connect', function (err) {
+        console.log('Cliente conectado -- sensor sin movimiento');
+        seqNo= seqNo+1;
+		console.log(seqNo);
+		// si el sensor alcanza el numero máximo de medidas definidas se cierra el programa
+		if (seqNo>maxMediciones) {
+			process.exit();
+		}
+		var medicion= {
+                "sensor" :{
+					"id":2,
+					"tipo":"SW420"
+				},
+				"dispositivo":{
+					"id":2,
+					"tipo":"Raspberry Pi 3 Model B+"
+				},
+                "fecha" : new Date(),
+                "medida ": {
+					"Movimiento": 0,
+                },
+                "alerta" : false,
+                "observaciones" : "Sin movimiento"
+            };
+            console.log("segNo: ", medicion); 
+            c1.write (medicion);
+
+        });
+               
+        c1.on('disconnect', function (err) {
+            console.log('Servidor desconectado o error en la coneccion');
+        });              
+
+            console.log('STARTED');
+	}
+	var data = JSON.stringify({ client: 'raspberry 2', status: status,  timestamp: new Date() });
+
+	console.log(data);
+	setTimeout(function(){ TestVibration() }, 200);
+};
+setTimeout(function(){ TestVibration() }, 6000);
